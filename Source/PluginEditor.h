f2eecc0d2add91101c5dabaa1e36b31155adf5f8
Tiago Inácio <tiago.inacio@farfetch.com>
2021-08-01 00:49:40 +0100

    feat: adds custom rotary sliders

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,33 +1,47 @@
 /*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file contains the basic framework code for a JUCE plugin editor.
+ 
+ ==============================================================================
+ */
 
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+  CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                      juce::Slider::TextEntryBoxPosition::NoTextBox)
+  {
+    
+  }
+};
+
 //==============================================================================
 /**
-*/
+ */
 class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
   SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
   ~SimpleEQAudioProcessorEditor() override;
-
+  
   //==============================================================================
   void paint(juce::Graphics &) override;
   void resized() override;
-
+  
 private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   SimpleEQAudioProcessor &audioProcessor;
-
+  
+  CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
+  
+  std::vector<juce::Component *> getComps();
+  
+  
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
