372cd7c161a827a882d2af0f67c99a5e4de6784e
tiago.inacio <tiago.inacio@farfetch.com>
2021-08-09 00:00:48 +0100

    feat: create ResponseCurveComponent

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -20,10 +20,27 @@ struct CustomRotarySlider : juce::Slider
   }
 };
 
+struct ResponseCurveComponent: juce::Component,
+  juce::AudioProcessorParameter::Listener,
+  juce::Timer {
+
+  ResponseCurveComponent(SimpleEQAudioProcessor &);
+  ~ResponseCurveComponent();
+  void parameterValueChanged(int parameterIndex, float newValue) override;
+  void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
+  void timerCallback() override;
+  void paint(juce::Graphics &g) override;
+
+private:
+    SimpleEQAudioProcessor &audioProcessor;
+    juce::Atomic<bool> parametersChanged { false };
+    MonoChain monoChain;
+};
+
 //==============================================================================
 /**
  */
-class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor, juce::AudioProcessorParameter::Listener, juce::Timer
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
   SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
@@ -33,20 +50,12 @@ public:
   void paint(juce::Graphics &) override;
   void resized() override;
   
-  void parameterValueChanged(int parameterIndex, float newValue) override;
-  void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
-  
-  void timerCallback() override;
-  
-  
 private:
-  // This reference is provided as a quick way for your editor to
-  // access the processor object that created it.
   SimpleEQAudioProcessor &audioProcessor;
-  
-  juce::Atomic<bool> parametersChanged { false };
-  
+
   CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
+
+  ResponseCurveComponent responseCurveComponent;
   
   using APVTS = juce::AudioProcessorValueTreeState;
   using Attachment = APVTS::SliderAttachment;
@@ -54,8 +63,6 @@ private:
   Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment, lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
   
   std::vector<juce::Component *> getComps();
-  
-  MonoChain monoChain;
-  
+
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
