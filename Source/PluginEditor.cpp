28e9d21bd44af247a07ddbbccb756bdcc06d35b3
tiago.inacio <tiago.inacio@farfetch.com>
2022-04-10 20:44:30 +0100

    feat: add frequency labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -302,9 +302,9 @@ void ResponseCurveComponent::resized() {
   Graphics g(background);
 
   Array<float> freqs {
-    20, 30, 40, 50, 100,
-    200, 300, 400, 500, 1000,
-    2000, 3000, 4000, 5000, 10000,
+    20, /*30, 40,*/ 50, 100,
+    200, /*300, 400,*/ 500, 1000,
+    2000, /*3000, 4000,*/ 5000, 10000,
     20000
   };
 
@@ -338,7 +338,36 @@ void ResponseCurveComponent::resized() {
     g.drawHorizontalLine(y, left, right);
   }
 
-  g.drawRect(getRenderArea());
+  g.setColour(Colours::lightgrey);
+  const int fontHeight = 10;
+  g.setFont(fontHeight);
+
+  for (int i = 0; i < freqs.size(); i++) {
+    auto f = freqs[i];
+    auto x = xs[i];
+    bool addK = false;
+    String str;
+    if (f > 999.f) {
+      addK = true;
+      f = f / 1000.f;
+    }
+
+    str << f;
+
+    if (addK) {
+      str << "k";
+    }
+    str << "Hz";
+
+    auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+    Rectangle<int> r;
+    r.setSize(textWidth, fontHeight);
+    r.setCentre(x, 0);
+    r.setY(1);
+
+    g.drawFittedText(str, r, juce::Justification::centred, 1);
+  }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getAnalysisArea() {
